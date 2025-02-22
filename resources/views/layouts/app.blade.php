dae87f499f5d76fa4fe654fa351774c7256a56bc
Azim Sofi <azimmiskandar@gmail.com>
2025-02-22 18:33:26 +0900

    修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,6 +15,7 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
         @include('layouts.navigation')
@@ -21,13 +23,13 @@
         <!-- Page Heading -->
         <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header ?? 'Header not found' }}
+                @yield('header')
             </div>
         </header>
 
         <!-- Page Content -->
         <main>
-            {{ $content ?? 'Content not found' }}
+            @yield('content')
         </main>
 
         <!-- Footer -->
@@ -38,4 +40,5 @@
         </footer>
     </div>
 </body>
+
 </html>
