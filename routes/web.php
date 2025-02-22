4cd55108a805009b74d3acd004541f8a80a72579
Azim Sofi <azimmiskandar@gmail.com>
2025-02-22 20:10:41 +0900

    修正（まだですけど）

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', ['App\Http\Controllers\TodoController', 'index']);
 
 Route::resource('todos', 'App\Http\Controllers\TodoController');
